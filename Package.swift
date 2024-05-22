d7d747d86845bd18b0cc1f0cd2537376f847cdf5
phuoc <phuoc@bitmark.com>
2024-05-22 17:26:20 +0700

    collection lib
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Core dependencies
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
         .package(name: "secp256k1", url: "https://github.com/BlockchainCommons/secp256k1-zkp.swift.git", from: "0.5.1"),
-        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.3")),
+        .package(url: "https://github.com/wolfmcnally/swift-collections", from: "1.1.0"),
         .package(url: "https://github.com/vapor/websocket-kit", .upToNextMajor(from: "2.6.1")),
 
         // PromiseKit dependency
@@ -42,7 +42,7 @@ let package = Package(
                 .product(name: "BigInt", package: "BigInt"),
                 .product(name: "secp256k1", package: "secp256k1"),
                 .product(name: "Collections", package: "swift-collections"),
-                .product(name: "WebSocketKit", package: "websocket-kit"),
+                .product(name: "WebSocketKit", package: "websocket-kit")
             ],
             path: "Sources",
             sources: ["Core", "FoundationHTTP"]),
